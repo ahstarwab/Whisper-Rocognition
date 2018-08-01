338cf3bf3da05792c6d5b143066887878931bf64
Minseok-Kang <ahstarwab@naver.com>
2018-08-01 11:30:45 +0900

    Minseok Changed Main Function

--- Learning_Model/Learning/git_practice.cpp
+++ Learning_Model/Learning/git_practice.cpp
@@ -5,10 +5,22 @@ double mul(double x, double y)
 	return x * y;
 }
 
+double div(double x, double y)
+{
+	return x / y;
+}
+
+
+
 int main() {
-	
+	int a, b;
+	cin >> a >> b;
+	cout << add(a,b)<<endl;
+	cout << sub(a,b)<<endl;
+	cout << mul(a,b)<<endl;
+	cout << div(a,b)<<endl;
 
 
 
-
+	return 0;
 }
