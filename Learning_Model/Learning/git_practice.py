f77670d5436c97bea91009d64f1a0b252dfe6843
root <eug92@naver.com>
2018-08-03 11:46:20 +0900

    new python pjt

---
+++ Learning_Model/Learning/git_practice.py
@@ -0,0 +1,31 @@
+"""
+	remote repository & main func - 용혁
+	mul & div (a, b) - 의협
+	add & sub (a, b) - 다운
+"""
+
+# 이하 mul(a, b) func 자리
+
+
+
+# 이하 div(a, b) func 자리
+
+
+
+# 이하 add(a, b) func 자리
+
+
+
+# 이하 sub(a, b) func 자리
+
+
+
+# 이하 main func
+
+a = float(input("1st arg : "))
+b = float(input("2nd arg : "))
+
+print("%s + %s = %s" %(a, b, add(a,b)))
+print("%s - %s = %s" %(a, b, sub(a,b)))
+print("%s x %s = %s" %(a, b, mul(a,b)))
+print("%s / %s = %s" %(a, b, div(a,b)))
