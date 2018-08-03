b7f6a77893ffa66e43cb8663d517a57799209908
root <eug92@naver.com>
2018-08-03 11:28:48 +0900

    Arrange project tree and add python pjt.

---
+++ Learning_Model/Learning/test_python/git_practice.py
@@ -0,0 +1,38 @@
+"""
+	remote repository 생성 및 main code - 용혁
+
+	mul(a, b) - 재빈
+	div(a, b) - 의협
+	sub(a, b) - 다운
+	add(a, b) - 재빈
+	
+	위 func을 담당자가  정의해주세요
+	conflict를 최대한 방지하기 위해, 함수 별 영역 지정을 하겠습니다.
+
+"""
+
+# 이하 mul(a, b) function 자리
+
+
+
+# 이하 div(a, b) function 자리
+
+
+
+# 이하 sub(a, b) function 자리
+
+
+
+# 이하 add(a, b) function 자리
+
+
+
+# 이하 main code
+
+a = float(input("1st number : "))
+b = float(input("2nd number : "))
+
+print("%s + %s = %s" %(a, b, add(a,b)))
+print("%s - %s = %s" %(a, b, sub(a,b)))
+print("%s x %s = %s" %(a, b, mul(a,b)))
+print("%s / %s = %s" %(a, b, div(a,b)))
